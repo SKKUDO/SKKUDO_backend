5aaac74274f4c41405e8304eef47e28118a2b985
taeju-moon <pp2lycee@naver.com>
2023-01-16 14:10:00 +0900

    refactor middlewares

--- src/middlewares/club.ts
+++ src/middlewares/club.ts
@@ -1,16 +1,19 @@
 import { Middleware } from '../types/common';
 import { Applier } from '../models/apply/Applier';
 
-export const isApplierExist: Middleware = (req, res, next) => {
-  const clubId = req.params.clubId;
-  Applier.find({ clubId }).then((data) => {
-    if (data.length > 0) {
-      res.status(403).json({
-        status: 'fail',
-        error: '모집공고가 있는 상태에서 club의 정보를 변경할 수 없습니다.',
-      });
-    } else {
-      next();
-    }
-  });
+export const isApplierExist: Middleware = async (req, res, next) => {
+  try {
+    const clubId = req.params.clubId;
+    const applier = await Applier.find({ clubId });
+    if (applier.length > 0)
+      res
+        .status(403)
+        .json({
+          status: 'fail',
+          error: '모집공고가 있는 상태에서 club의 정보를 변경할 수 없습니다.',
+        });
+    else next();
+  } catch (error: any) {
+    res.status(500).json({ status: 'fail', error: error.message });
+  }
 };
