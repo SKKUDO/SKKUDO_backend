cdbd808869a104cec2cc4716b863f8bd0054c645
taeju-moon <pp2lycee@naver.com>
2023-01-20 17:44:23 +0900

    please

--- src/controllers/club/Club.ts
+++ src/controllers/club/Club.ts
@@ -101,7 +101,7 @@ export const acceptClub: Controller = async (req, res) => {
     }
     club.accepted = true;
     const initializer: string = club.initializer as string;
-    const user = await User.findOne({ _id: initializer });
+    const user = await User.findOne({ userID: initializer });
     //2. 설립자를 회장으로 등록한다.
     if (!user) {
       res.status(404).json({
